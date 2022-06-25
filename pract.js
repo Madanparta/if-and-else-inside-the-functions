a23d2b88a0fec37d8e1800b57b2c3052f6fce559
madan k <partagowda15@gmil.com>
2022-06-26 02:08:21 +0530

    adding some questions.

--- pract.js
+++ pract.js
@@ -1,5 +1,123 @@
-let fun = (a,b)=>{
-    let c = Math.max(a,b);
-    return c;
-  }
-  console.log(fun(10,56))
+// 1.TWO NUMBERS AND RETURN THE BIGGER.
+// ANSWER.
+// let fun = (a,b)=>{
+//     let c = Math.max(a,b);
+//     return c;
+//   }
+//   console.log(fun(10,56))
+
+// 2.ODD AND EVEN NUMBER
+// ANSWER.
+// let oddEven = (a)=>{
+//     if(a%2==0){
+//       return 'ODD';
+//     }else{
+//       return 'EVEN';
+//     }
+//   }
+// console.log(oddEven(22))
+
+
+// 3.POSITIVE NUMBER AND NEGETIVE NUMBERS 
+// ANSWER.
+// let checkinNumber = (a)=>{
+//     if(a>0)return "POSITIVE";return "NEGITIVE";
+//   }
+// console.log(checkinNumber(0))
+
+// 4.DIVISIBLE BY 3 AND 5
+// ANSWER
+// let dividing=(a)=>{
+//     if(a%3==0 || a%5==0)return "YES"; return "NO";
+//   }
+//   console.log(dividing(7))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 5.ABSOLUTE VALUE
+// ANSWER.
+// let obsoluteVlue =(n)=>{
+//     let c = Math.abs(n);
+//     if(n>0)return n;return c;
+//   }
+//   console.log(obsoluteVlue(-4))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 6.THIREE NUMBER FIND THE BIGEST.
+// ANSWER.
+// let bigInThree =(a,b,c)=>{
+//     let takeABig = Math.max(a,b,c);
+//     if(a>0 && b>0 && b>0)return (takeABig); return "sortu";
+//   }
+//   console.log(bigInThree(10,56,890));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 7.FOUR NUMBER FIND BIGEST.
+// ANSER.
+// let bigInFour = (a,b,c,d)=>{
+//     let forIntBig = Math.max(a,b,c,d);
+//     if(a+b+c+d > 0)return (forIntBig);return ("Entering bellow number,")
+//   }
+//   console.log(bigInFour(67,45,90,00));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 8.GIVEN THE THREE NUMBER FIND SECOUND SMALLEST NUMBER
+// ANSWER.
+// let secoundNum = (a,b,c)=>{
+//     if(a>b && a>c)return b; return c;
+//     if(b>a && b>c)return a; return b;
+//     if(c>a && c>b)return a; return b;
+//   }
+//   console.log(secoundNum(12,43,22))
+  
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 9.FINDING ANGLES.
+// ANSWER.
+
+// let angleOfTringle = (aBase,bLeft,cRight)=>{
+//     // if(aBase+bLeft+cRight === 180){
+//     //   return "VALID";
+//       if(aBase == 60 && bLeft == 60 && cRight== 60 ){
+//         let v= "VALID";
+//         if(aBase+bLeft+cRight == 180 )return (v+" EQUILATERAL");
+//       }
+//       if(aBase <= 70 && bLeft <= 70 && cRight >= 40){
+//         let u= "VALID";
+//         if(aBase+bLeft+cRight == 180 )return (u+" ISOSCELES");
+//       }
+//       if(aBase <= 90 && bLeft <= 75 && cRight<= 45){
+//         let y = "VALID";
+//         if(aBase+bLeft+cRight == 180 )return (y+" SCALENE");
+//       }
+//       else{
+//         let e= "NOT VALID";
+//         return (e+" Do PROPER ANGLES..")
+//       }
+  
+//   }
+//   console.log(angleOfTringle(80,65,35))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 10.LEAP YEAR.
+// ANSWER.
+// let leapYear = (year)=>{
+//     if(year%4==0){
+//       if(year%100==0){
+//         if(year%400==0){
+//           return(year+" YES");
+//         }else{
+//           return(year+" NOT");
+//         }
+//       }else{
+//         return (year+" YES");
+//       }
+//     }else{
+//       return (year+" NOT");
+//     }
+//   }
+//   console.log(leapYear(2056))
---
+++ readme.md
@@ -0,0 +1,41 @@
+# If and Else in side the Functions.
+
+`1.`Take two numbers a,b and return the bigger number.
+
+---
+
+`2.`Take a number and return "ODD" if the number is odd and "EVEN" if the number is even.
+
+---
+
+`3.`Take an integer as input and return "POSITIVE" if the integer is positive, "NEGATIVE" if the integer is negative and "ZERO" if the integer is zero.
+
+---
+
+`4.`Take an integer and return "YES" if the input is divisible by 3 or 5 or both. "NO" in all other cases
+
+---
+
+`5.`Take an integer and return its absolute value.
+
+---
+
+`6.`Take 3 distinct integers a,b,c and return the integer of largest value.
+
+---
+
+`7.`Take 4 distinct integers a,b,c, d and return the integer of largest value.
+
+---
+
+`8.`Take 3 distinct integers a,b,c as input and return the second smallest among the input.
+
+---
+
+`9.`Take 3 integers A,B,C which denote the angles of a triangle, and return 'VALID' if the angles can form a valid triangle or 'NOT VALID' if the angles cannot form a valid triangle. Print the type of triangle in case it is valid
+
+---
+
+`10.`Take a 4 digit integer denoting a year and print out "YES" if it is a leap year and "NO" if it is not a leap year.
+
+---
